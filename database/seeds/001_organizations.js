2393fafda6646337c10d094679c3cc8e37ede3aa
Min-Huang <minh.huang96@gmail.com>
2019-10-24 09:55:22 -0700

    readme.md

--- database/seeds/001_organizations.js
+++ database/seeds/001_organizations.js
@@ -4,9 +4,9 @@ exports.seed = function(knex) {
     
       
       return knex("organizations").insert([
-        { id: 1, organ_name: "African Wildlife Foundation" },
-        { id: 2, organ_name: "Clearwater Marine Foundation" },
-        { id: 3, organ_name: "African Animals Resucue" }
+        {  organ_name: "African Wildlife Foundation" },
+        {  organ_name: "Clearwater Marine Foundation" },
+        {  organ_name: "African Animals Resucue" }
       ]);
     
 };
